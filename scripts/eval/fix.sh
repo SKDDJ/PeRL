a0c6bf26cbce60e8fff8e43f11435d984f7e90ce
MikaStars39 <meloxdmy@qq.com>
2025-12-02 17:15:11 +0800

    [script] merged scripts

--- scripts/eval/fix.sh
+++ scripts/eval/fix.sh
@@ -1,13 +1,2 @@
 python debug.py \
-    outputs/train/dapo_dora_qwen2_5_1_5b_20251126_115730 \
-    outputs/train/dapo_ia3_qwen2_5_1_5b_20251128_120647 \
-    outputs/train/dapo_layernorm_qwen2_5_1_5b_20251127_195534 \
-    outputs/train/dapo_lora_lr5_20251129_222821 \
-    outputs/train/dapo_lora_r16_qwen2_5_3b_20251124_104900 \
-    outputs/train/dapo_lora_r8_20251129_135342 \
-    outputs/train/dapo_milora_qwen2_5_1_5b_20251126_224006 \
-    outputs/train/dapo_miss_qwen_1_5b \
-    outputs/train/dapo_miss_qwen2_5_1_5b_20251124_220354 \
-    outputs/train/dapo_pissa_qwen2_5_1_5b_20251126_192154 \
-    outputs/train/dapo_vera_qwen2_5_1_5b_20251126_190555 \
-    outputs/train/dr_grpo_lora_20251129_132413
+    outputs/train/dapo_lora_lr5_20251130_193154
